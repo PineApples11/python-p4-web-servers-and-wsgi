59b516d6691540a74365a9505d2da32d5debe566
PineApples11 <kinyanjuiivy1@gmail.com>
2025-04-03 13:00:18 +0300

    WSGI UNDERSTANDING

--- server/werkzeug_app.py
+++ server/werkzeug_app.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+from werkzeug.wrappers import Request,Response
+
+@Request.application
+def application(request):
+  print(f'This web server is running at {request.remote_addr}')
+  return  Response('A WSGI generated this response!')
+
+if __name__ == '__main__':
+
+  # ensures that certain code only runs when the script is executed directly, not when it's imported.
+  from werkzeug.serving import run_simple
+  run_simple(
+    hostname='localhost',
+    port=5555,
+    application=application
+  )
+
